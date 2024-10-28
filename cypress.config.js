cdb2cc50a89e24eff730100eaead6cd382962ff6
ankitsahay <142355935+ankitraunak@users.noreply.github.com>
2024-10-28 16:49:09 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.js", // Adjust this pattern as per your test files
+    specPattern: "cypress/e2e/saucedemo.spec.cy.js", // Adjust this pattern as per your test files
     supportFile: false,
   },
 });

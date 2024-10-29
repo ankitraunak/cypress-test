295364a53c95ee630120158d8800ea98cfc87dcc
ankitsahay <142355935+ankitraunak@users.noreply.github.com>
2024-10-29 11:21:56 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/saucedemo.spec.cy.js", // Adjust this pattern as per your test files
+    specPattern: "cypress/e2e/saucedemo.spec.cy.js", 
     supportFile: false,
+    video: true, // Enable video recording of tests
+    screenshotsFolder: "cypress/screenshots", // Folder for screenshots
+    videosFolder: "cypress/videos", // Folder for videos
+    reporter: "mochawesome", // Use mochawesome as the reporter
+    reporterOptions: {
+      reportDir: "cypress/report/mochawesome-report", // Directory for the report
+      overwrite: false,
+      html: true,
+      json: true,
+    },
   },
 });
